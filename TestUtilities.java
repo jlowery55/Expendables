17edee23afd09f3db3489924ee199f0035ba2fad
richteas <60367685+richteas@users.noreply.github.com>
2021-04-29 16:44:32 -0700

    Deleted more of joey's lab

--- TestUtilities.java
+++ TestUtilities.java
@@ -129,57 +129,7 @@ public class TestUtilities {
 		}
 		return i;
 	}
-
-	// open the default database;
-	static void openDefault() {
-		testObj.openDB();
-	}
-
-	// test getNameSalary() method
-	static void callGetNameSalary() throws SQLException {
-		ResultSet rs;
-		System.out.println("Research Department Employees");
-		System.out.println("*****************************");
-		System.out.printf("LastName, FirstName        Salary\n");
-		rs = testObj.getNameSalary();
-		while (rs.next()) {
-			System.out.printf("%-26s %s \n", rs.getString(1) + ", " + rs.getString(2), 
-					                         rs.getString(3));
-		}
-	}
-
-	// test matchName() method
-	static void callMatchName() throws SQLException {
-		ResultSet rs;
-		String target;
-		target = "K";
-		System.out.println("\nEmployees with name that starts with " + target);
-		System.out.println("***************************************************");
-		System.out.printf("%-12s  %s\n", "Dept Number",   "LastName, FirstName");
-		rs = testObj.matchLastName(target);
-		while (rs.next()) {
-			System.out.printf("    %-8s    %s\n", rs.getString(1), 
-					rs.getString(2) + ", " + rs.getString(3));
-		}
-	}
 	
-	 
-	//test employeeByDNO() method 
-	static void callEmployeeByDNO() throws SQLException {
-		ResultSet rs;
-		System.out.print("Please enter a department number: ");
-		String input = keyboard.nextLine();
-		int dno= Integer.parseInt(input); 
-		System.out.println("\nEmployees that work in department " + dno); 
-		System.out.println("*******************************************");
-		System.out.printf("%-12s   %s\n", "Dept Number",   "LastName, FirstName");
-		rs = testObj.employeeByDNO(dno); 
-		while(rs.next()){ 
-			System.out.printf("    %-8s     %s\n", rs.getString(1), 
-					rs.getString(2) + ", " + rs.getString(3));
-			}
-		
-	}
 	//test new Method1
 	static void openUserpass() {
 		testObj.openDB("ex367", "ex367");
